2e15b034928563e33e4299d7a40fbce125a20959
Hecht Fabio, INI-DNA-DL <Fabio.Hecht2@swisscom.com>
2020-12-02 17:34:56 +0100

    no cast

--- src/main/java/io/confluent/connect/jdbc/source/JdbcSourceTask.java
+++ src/main/java/io/confluent/connect/jdbc/source/JdbcSourceTask.java
@@ -21,19 +21,14 @@ import com.cronutils.model.definition.CronDefinition;
 import com.cronutils.model.definition.CronDefinitionBuilder;
 import com.cronutils.model.time.ExecutionTime;
 import com.cronutils.parser.CronParser;
-
-import java.time.Instant;
-import java.time.ZoneId;
-import java.time.ZoneOffset;
-import java.time.ZonedDateTime;
-import java.util.Optional;
-import java.util.TimeZone;
 import org.apache.kafka.common.config.ConfigException;
 import org.apache.kafka.common.utils.SystemTime;
 import org.apache.kafka.common.utils.Time;
 import org.apache.kafka.connect.data.Schema;
 import org.apache.kafka.connect.data.SchemaAndValue;
 import org.apache.kafka.connect.errors.ConnectException;
+import org.apache.kafka.connect.header.ConnectHeaders;
+import org.apache.kafka.connect.header.Headers;
 import org.apache.kafka.connect.source.SourceRecord;
 import org.apache.kafka.connect.source.SourceTask;
 import org.slf4j.Logger;
@@ -41,6 +36,10 @@ import org.slf4j.LoggerFactory;
 
 import java.sql.Connection;
 import java.sql.SQLException;
+import java.time.Instant;
+import java.time.ZoneId;
+import java.time.ZoneOffset;
+import java.time.ZonedDateTime;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Calendar;
@@ -50,8 +49,10 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Locale;
 import java.util.Map;
+import java.util.Optional;
 import java.util.PriorityQueue;
 import java.util.Set;
+import java.util.TimeZone;
 import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.function.Function;
 import java.util.stream.Collectors;
@@ -414,17 +415,25 @@ public class JdbcSourceTask extends SourceTask {
         boolean hadNext = true;
         while (results.size() < batchMaxRows && (hadNext = querier.next())) {
           int rowIndex = querier.resultSet.getRow(); // 1 for first row, 2 for second one
+
           SourceRecord record = querier.extractRecord();
-          record.headers().add("sbd.batch.id", new SchemaAndValue(Schema.STRING_SCHEMA, batchId));
-          record.headers().add("sbd.batch.size", new SchemaAndValue(Schema.INT32_SCHEMA, batchSize));
-          record.headers().add("sbd.batch.index", new SchemaAndValue(Schema.INT32_SCHEMA, rowIndex));
-          ((Map<String, Object>) record.sourceOffset()).put("sbd.batch.id", batchId);
+
+          Map<String, Object> sourceOffset = new HashMap<>(record.sourceOffset());
+          sourceOffset.put("sbd.batch.id", batchId);
+
+          Headers headers = new ConnectHeaders(record.headers());
+          headers.add("sbd.batch.id", new SchemaAndValue(Schema.STRING_SCHEMA, batchId));
+          headers.add("sbd.batch.size", new SchemaAndValue(Schema.INT32_SCHEMA, batchSize));
+          headers.add("sbd.batch.index", new SchemaAndValue(Schema.INT32_SCHEMA, rowIndex));
           if (rowIndex == batchSize) {
             final String batchEndTime = Instant.now().atZone(zone).toString();
-            record.headers().add("sbd.batch.started.at", new SchemaAndValue(Schema.STRING_SCHEMA, batchStartTime));
-            record.headers().add("sbd.batch.completed.at", new SchemaAndValue(Schema.STRING_SCHEMA, batchEndTime));
+            headers.add("sbd.batch.started.at", new SchemaAndValue(Schema.STRING_SCHEMA, batchStartTime));
+            headers.add("sbd.batch.completed.at", new SchemaAndValue(Schema.STRING_SCHEMA, batchEndTime));
           }
-          results.add(record);
+
+          results.add(new SourceRecord(record.sourcePartition(), sourceOffset, record.topic(),
+              record.kafkaPartition(), record.keySchema(), record.key(), record.valueSchema(),
+              record.value(), record.timestamp(), headers));
         }
 
         if (!hadNext) {
